cc7f740bbd544cb8560d13620e99361d617e014a
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2021-12-11 00:11:18 -0800

    testing

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
+    //this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('sky', 'black_background.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
 }
